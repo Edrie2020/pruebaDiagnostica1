8ce47f1624f5bbf35dbb9c1e85dc9e20fb6e812f
edrie <rudribe81@gmil.com>
2021-08-11 19:43:16 -0400

    Se agrega el archivo Example1.java que corresponde al ejercicio de parte 2.

--- pares/Example2.java
+++ pares/Example2.java
@@ -1,14 +1,21 @@
-package com;
-
+/*
+*   Parte 2
+*	Deberan crear un programa, el cual debera recibir un parametro n ingresado por el usuario, 
+*   y mostrar los primeros n pares.
+*/
 public class Example2 {
 	public static void main(String[] args) {
 		int n = 6;
-		Example2.mostrarNumerosPares1(n);
+		Example2.mostrarNumerosImpares(n);
 	}
-	public static void mostrarNumerosPares1(int n) {
+	public static void mostrarNumerosImpares(int n) {
+		//establecemos el limite del for, sumamos 1 para limitar el for a 13 ya que el 
+		//0 corresponde a la posicion incial que es cero
 		int limite = (n*2 + 1);
 		for(int i = 0; i < limite ; i++) {
+			//condicion que verifica si un numero es par
 			if(i % 2 == 0) {
+				//Se verifica que el numero par sea mayor a 0 para imprimirlo por consola
 				if(i > 0)
 					System.out.println(i);
 			}
