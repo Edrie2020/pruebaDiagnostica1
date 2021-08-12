d32f8b694db900a2947f27fbc6544df7c4699e6d
edrie <rudribe81@gmil.com>
2021-08-11 20:07:02 -0400

    Se agrega el archivo Example4.java que corresponde al ejercicio de parte 2. de suma de impares.

--- impares/Example4.java
+++ impares/Example4.java
@@ -1,9 +1,15 @@
-package com;
-
+/*
+*   Parte 2
+*	Deberan crear un programa, donde el usuario ingresa dos valores, el limite 
+*   inferior(min) y superior(max) del intervalo para realizar la suma
+*   de los impares. 
+*/
 public class Example4 {
 	public static void main(String[] args) {
 		int n = 6;
+		//rango minimo
 		int min = 6;
+		//rango max
 		int max = 30;
 		int sumatoria = Example4.mostrarNumerosPares1(n, min, max);
 		System.out.println(sumatoria);
@@ -11,7 +17,9 @@ public class Example4 {
 	public static int mostrarNumerosPares1(int n,  int min,  int max) {
 		int sumatoria = 0;
 		for(int i = 0; i < 100 ; i++) {
+			//Se verifica que el numero sea impar.
 			if(!(i % 2 == 0 )) {
+				//S valida que el numero impar 'i' este en el rango min y maximo
 				if((  i > min  &&  i < max )) {
 					sumatoria = sumatoria + i;
 				}
