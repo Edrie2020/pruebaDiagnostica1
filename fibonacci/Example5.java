a75b6151d9e36e89a34c901cbc02705ace91941a
edrie <rudribe81@gmil.com>
2021-08-11 20:15:17 -0400

    Se agrega el archivo Example5.java que corresponde al ejercicio de parte 1. de Fibonnaci

--- fibonacci/Example5.java
+++ fibonacci/Example5.java
@@ -1,9 +1,16 @@
-package com;
+/*
+*   Secuencia de Fibonacci.
+*	
+*	Se debe crear un programa que reciba un numero ingresado por el usuario,
+*	que indique cuantos elementos de la serie de Fibonacci, y el programa debe mostrar
+*   los primeros n numeros de la secuencia de Fibonacci. 	
+*/
 public class Example5 {
 	public static void main(String[] args) {
 		int n = 6;
 		for (int i = 1; i <= n; i++) {
 			int numero = Example5.fibonacci(i);
+			//se imprime el numero.
 			System.out.println(numero);
 		}
 	}
